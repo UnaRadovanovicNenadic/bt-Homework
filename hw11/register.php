b37ac799c9775fea14dace9d4023d8bfba137461
Una Radovanovic Nenadic <una.radovanovic@gmail.com>
2022-04-28 14:46:45 +0200

    check

--- hw11/register.php
+++ hw11/register.php
@@ -7,17 +7,17 @@
     } elseif ($gender = "female") {
         echo "Miss $name $surname,";
     } else {
-        echo "Mister/Miss $name $surname,":
+        echo "Mister/Miss $name $surname,";
     }
     echo "<br>";
     echo "You have successfully registered for our course(s).";
     echo "<br>";
     $password = $_GET['password'];
-    echo "Your password is: $password."
+    echo "Your password is: $password.";
     echo "<br>";
     $email = $_GET['email'];
     echo "Your username is: $email.";
     echo "<br>";
-    $course_list = $_GET[array ($c_choice)];
+    $course_list = $_GET[array($c_choice)];
     echo "Your chosen courses are: $corse_list."
 ?>
