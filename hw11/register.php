9ac88a4563ea4cf1d725d02172254b7873f52f91
Una Radovanovic Nenadic <una.radovanovic@gmail.com>
2022-04-28 18:29:16 +0200

    php reset

--- hw11/register.php
+++ hw11/register.php
@@ -1,71 +1,10 @@
-<!DOCTYPE html>
-<html>
 
+<?php
 
-<head>
-    <title>Register</title>
-    <link rel="stylesheet" href="./theme/CSS/register.css">
-</head>
+    $n1 = $_GET["n1"];
+    $n2 = $_GET["n2"]]
+    echo $n1;
+    echo $n2;
+    
 
-<body>
-
-    <header>
-
-        <div id="headline">
-            <h1 class="head">Registration Confirmation</h1>
-        </div>
-
-        <div>
-        <nav id="nav">
-            <ul type="none">Web Page Navigation:
-                <a href="register.html">Register</a> > <a href="register.php">Registration</a>
-            </ul>
-        </nav>
-        </div>
-
-    </header>
-
-    <main>
-
-        <?php
-            $name = $_GET["ime"];
-            echo $name;
-
-
-        ?>
-
-        <?php
-            $name = $_GET['name'];
-            $surname = $_GET['surname'];
-            $gender = array($_GET['gender']);
-            if ($gender == "male") {
-                echo "Mister $name $surname,";
-            } elseif ($gender == "female") {
-                echo "Miss $name $surname,";
-            } else {
-                echo "Mister/Miss $name $surname,";
-            }
-            echo "<br>";
-            echo "You have successfully registered for our course(s).";
-            echo "<br>";
-            $password = $_POST['password'];
-            echo "Your password is: $password.";
-            echo "<br>";
-            $email = $_POST['email'];
-            echo "Your username is: $email.";
-            echo "<br>";
-            $course_list = $_POST[array($c_choice)];
-            echo "Your chosen courses are: $corse_list."
-        ?>
-
-    </main>
-
-    <footer>
-
-        <h1 class="foot">Thank <em>YOU</em> for your TRUST!
-        <br>
-        Now, You Can Learn Easy and Quickly with <em>OUR</em> HELP!</h1>
-
-    </footer>
-</body>
-</html>
+?>
