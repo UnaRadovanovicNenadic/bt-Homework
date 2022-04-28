a6e5c2112d379e54f84cb07b1206735f42a4fd06
Una Radovanovic Nenadic <una.radovanovic@gmail.com>
2022-04-28 14:38:56 +0200

    add all as solution for not accepting add register.php

--- hw11/register.php
+++ hw11/register.php
@@ -1,4 +1,7 @@
 <?php
+    $name = $_GET['name'];
+    $surname = $_GET['surname'];
+    $gender = $_GET[array ("male", "female", "other")];
     if ($gender = "male") {
         echo "Mister $name $surname,";
     } elseif ($gender = "female") {
@@ -9,10 +12,12 @@
     echo "<br>";
     echo "You have successfully registered for our course(s).";
     echo "<br>";
+    $password = $_GET['password'];
     echo "Your password is: $password."
     echo "<br>";
+    $email = $_GET['email'];
     echo "Your username is: $email.";
     echo "<br>";
-    $course_list = array ($c_choice);
+    $course_list = $_GET[array ($c_choice)];
     echo "Your chosen courses are: $corse_list."
 ?>
