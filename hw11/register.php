7526ee3d50526ec3cebb7abc376f1c70cca63bd7
Una Radovanovic Nenadic <una.radovanovic@gmail.com>
2022-04-28 16:32:36 +0200

    something is wrong

--- hw11/register.php
+++ hw11/register.php
@@ -1,7 +1,8 @@
 <?php
     $name = $_GET['name'];
+    print_r($name);
     $surname = $_GET['surname'];
-    $gender = $_GET[array ('gender')];
+    $gender = $_GET['gender'];
     if ($gender = "male") {
         echo "Mister $name $surname,";
     } elseif ($gender = "female") {
@@ -12,12 +13,12 @@
     echo "<br>";
     echo "You have successfully registered for our course(s).";
     echo "<br>";
-    $password = $_GET['password'];
+    $password = $_POST['password'];
     echo "Your password is: $password.";
     echo "<br>";
-    $email = $_GET['email'];
+    $email = $_POST['email'];
     echo "Your username is: $email.";
     echo "<br>";
-    $course_list = $_GET[array($c_choice)];
+    $course_list = $_POST[array($c_choice)];
     echo "Your chosen courses are: $corse_list."
 ?>
