0316b1cc6165d87cb6c6018b451d0f609640139f
Una Radovanovic Nenadic <una.radovanovic@gmail.com>
2022-05-07 00:40:54 +0200

    task no5

---
+++ hw12/contact-us.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="zadatak5.css">
+    <title>Document</title>
+</head>
+<body>
+
+    <header>
+        <nav class="navigation">
+            <ul type="none">
+                <li><a href="">HOME</a></li>
+                <li><a href="">PRODUCTS</a></li>
+                <li><a href="">ABOUT US</a></li>
+                <li><a href="">CONTACT US</a></li>
+            </ul>  
+        </nav>  
+    </header>   
+
+    <main>
+    </main>
+
+    <footer class="footer_style">
+        <div class="footer1">
+            <div class="footer_blocks">
+                <p class="footers_heading">Company information</p>
+                <ul type="">
+                    <li><a href="#" class="a_foot">About us</a></li>
+                    <li><a href="#" class="a_foot">Contact us</a></li>
+                    <li><a href="#" class="a_foot">Contact us</a></li>
+                    <li><a href="#" class="a_foot">Company data</a></li>
+                </ul>  
+            </div>
+            
+            <div class="footer_blocks">
+                <p class="footers_heading">Customer support</p>
+                <ul type="">
+                    <li><a href="#" class="a_foot">How to order?</a></li>
+                    <li><a href="#" class="a_foot">How to create user profile?</a></li>
+                    <li><a href="#" class="a_foot">Payment methods</a></li>
+                    <li><a href="#" class="a_foot">Shipping services</a></li>
+                    <li><a href="#" class="a_foot">Reclamations</a></li>
+                    <li><a href="#" class="a_foot">Refund</a></li>
+                </ul>
+            </div>   
+
+            <div class="footer_blocks">
+                <p class="footers_heading">Terms of using</p>
+                <ul type="">
+                    <li><a href="#" class="a_foot">General terms of sale in online shop</a></li>
+                    <li><a href="#" class="a_foot">Terms of using online shop</a></li>
+                    <li><a href="#" class="a_foot">Privacy policy and data protection</a></li>
+                    <li><a href="#" class="a_foot">Cookie policy</a></li>
+                </ul>
+            </div>   
+            
+            <div class="footer_blocks">
+                <p class="footers_heading"><strong>COMPANY NAME</strong></p>
+                <ul type="">
+                    <li>Adresa, Beograd</li>
+                    <li>Telefon: 011/123-45-67</li>
+                    <li>PIB: 1234567</li>
+                    <li>MB: 12345678</li>
+                    <li>Account No.: 160-1234567891011-60</li>
+                </ul>  
+            </div>
+
+            <div class="footer_blocks">
+                <p class="footers_heading">FOLLOW US</p>
+                <div class="footer_subblock">
+                    <ul type="">
+                        <li><a href="" class="a_foot">Facebook</a></li>
+                        <li><a href="" class="a_foot">Instagram</a></li>
+                        <li><a href="" class="a_foot">Twiiter</a></li>
+                        <li><a href="" class="a_foot">Linked In</a></li>
+                    </ul>  
+                </div>
+
+                <div class="footer_subblock">
+                    <img src="" width="20px" alt="facebook_icon" class="img_foot">
+                    <img src="" width="20px" alt="instagram_icon" class="img_foot">
+                    <img src="" width="20px" alt="twitter_icon" class="img_foot">
+                    <img src="" width="20px" alt="linkedin_icon" class="img_foot">
+                </div>  
+            </div>
+
+            <div class="copy">
+
+                <div class="copy1">
+                    <p class="copy1_par">Copyright by URN</p>
+                </div>
+
+                <div class="copy2">                    
+                    <p class="copy2_par">Powered by IT bootcamp</p>
+                    <img src="" alt="itbootcamp" class="boot_logo">
+                </div>
+
+            </div>
+
+    </footer>
+
+    
+</body>
+</html>
